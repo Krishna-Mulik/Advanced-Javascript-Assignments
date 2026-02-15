cf218dedef265fc055a93cfc3d2838cec9dbc321
Krishna-Mulik <krishnamulik24@gmail.com>
2026-02-15 11:30:51 +0530

    optimized once callback fn

--- callbacks/easy/once.js
+++ callbacks/easy/once.js
@@ -6,27 +6,37 @@
 // Any subsequent calls should not re-execute `fn` and should instead invoke
 // the callback with the same result (or error) from the first invocation.
 
+//the once function uses the concept of in-flight deduplication.
+//in-flight: in progress (eg. fetch which is not completed).
+//deuplication: preventing duplicate work (eg. prevention of same API call)
+
 function once(fn) {
-  let isFnCalled = false;
-  let fnResponse = [true];
+  let isFinished = false;
+  let isCalled = false;
+  let result = null;
+  let waitingCallbacks = [];
 
   return (...args) => {
-    const actualcb = args.pop();
+    const cb = args.pop();
 
-    if (isFnCalled) {
-      actualcb(...fnResponse);
+    if (isFinished) {
+      cb(...result);
       return;
     }
 
-    isFnCalled = true;
-    const cb = (err, data) => {
-      fnResponse = [err, data];
-      actualcb(err, data);
-    };
+    waitingCallbacks.push(cb);
 
-    args.push(cb);
+    if (isCalled) return;
+    isCalled = true;
 
-    fn(...args);
+    fn(...args, (err, data) => {
+      isFinished = true;
+      result = [err, data];
+
+      for (const callback of waitingCallbacks) {
+        callback(err, data);
+      }
+    });
   };
 }
 
